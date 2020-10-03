b14ef7616e07983d744bb0692095ad83d79f228c
admin <romain.molle4@gmail.com>
2020-10-03 16:22:40 +0200

    first commit

---
+++ app/src/main/java/com/example/pts3/ItemPersonAdapter.java
@@ -0,0 +1,77 @@
+package com.example.pts3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ItemPersonAdapter extends RecyclerView.Adapter<ItemPersonAdapter.ItemPersonViewHolder> {
+    private ArrayList<ItemPerson> mItemPersonList;
+    public OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onAddClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener= listener;
+    }
+
+
+    public static class ItemPersonViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mText;
+        public ImageView mImageView2;
+
+        public ItemPersonViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imagePerson);
+            mText = itemView.findViewById(R.id.nomInvit);
+            mImageView2 = itemView.findViewById(R.id.imageView19);
+
+            mImageView2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onAddClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemPersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_person, parent, false);
+        ItemPersonViewHolder ipvh = new ItemPersonViewHolder(v, mListener);
+        return ipvh;
+    }
+    public ItemPersonAdapter(ArrayList<ItemPerson> itemlist){
+        mItemPersonList = itemlist;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ItemPersonViewHolder holder, int position) {
+        ItemPerson currentItem = mItemPersonList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mText.setText((currentItem.getNom()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemPersonList.size();
+    }
+
+}
