fc32bf16cb302a02861f6aaa432066f6b2547d7a
Thibault <thibaultderouin5@gmail.com>
2020-10-03 12:10:20 +0200

    Groupes

---
+++ app/src/main/java/com/example/pts3/PagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.pts3;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int numOfTabs){
+
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new EtudiantsFragment();
+            case 1:
+                return new GroupesFragment();
+            case 2:
+                return new QuizzFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
